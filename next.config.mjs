c458820c954c0a31628a1723ed0e22c407919ed2
UmbraFlare-code <umaxuel@gmail.com>
2025-12-04 18:03:33 -0500

    Update pague config next js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 const nextConfig = {
+  experimental: {
+    turbo: false,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -7,7 +10,6 @@ const nextConfig = {
   },
   output: 'export',
   trailingSlash: true,
-  // Cambia 'tu-repositorio' por el nombre real de tu repo en GitHub
   basePath: process.env.NODE_ENV === 'production' ? '/Calendario' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/Calendario/' : '',
 }
